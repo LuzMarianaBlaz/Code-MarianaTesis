5951ac25164940b8c85b92f38108cc65d0fd3bf3
Mariana Blaz <luzblaz@ciencias.unam.mx>
2021-02-26 17:35:07 -0600

    pruebas

--- code/python_code/__main__.py
+++ code/python_code/__main__.py
@@ -5,25 +5,29 @@ if __name__ == '__main__':
 
     r = redes.Red()
 
-    r.agrega_nodo('a')
-    r.agrega_nodo('b')
-    r.agrega_nodo('c')
-    r.agrega_nodo('d')
-    r.agrega_nodo('e')
-    r.agrega_nodo('f')
+    r.agrega_nodo('a',[0,2])
+    r.agrega_nodo('b',[1,0])
+    r.agrega_nodo('c',[0,1])
+    r.agrega_nodo('d',[1,1])
+    r.agrega_nodo('e',[0,0])
+    r.agrega_nodo('f',[7,2])
 
-    r.agrega_arista('a,c', 5., 7., 3)
-    r.agrega_arista('c,d', 5., 7., 4)
-    r.agrega_arista('d,b', 5., 7., 2)
-    r.agrega_arista('b,a', 5., 7., 1)
-    r.agrega_arista('d,e', 5., 7., 6)
-    r.agrega_arista('e,d', 5., 7., 2)
+    r.agrega_arista('a,c', 5., 1., 3)
+    r.agrega_arista('c,d', 5., 1., 4)
+    r.agrega_arista('d,b', 5., 1., 2)
+    r.agrega_arista('b,a', 5., 2.236, 1)
+    r.agrega_arista('d,e', 5., 1.4142, 6)
+    r.agrega_arista('e,d', 5., 1.4142, 2)
 
     for v in r.nodos:
         print(v,r.nodos[v].vecinos)
 
     for par in r.aristas:
         print(par, r.aristas[par].capacidad)
+    print('prueba en la red pequenia')
+    result = A_star(r,tiempo_euclideano,'a','f')
+    print(result)
+
 
     rc = redes.genera_red_cuadrada(3,[1.,0.,],1.,[2,0],mapa=True)
     for v in rc.nodos:
@@ -33,6 +37,6 @@ if __name__ == '__main__':
         print(par, rc.aristas[par].max_vel, rc.aristas[par].longitud, rc.aristas[par].capacidad)
     print(rc.ismap)
 
-    #TODO: Pruebas del algoritmo base
+    print('prueba en red cuadrada')
     result = A_star(rc,tiempo_euclideano,'0','6')
     print(result)
