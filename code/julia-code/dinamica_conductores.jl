5ab73dfe631a5eafcc7f079272e867f4306d6df4
Mariana Blaz <luzblaz@ciencias.unam.mx>
2021-04-18 12:38:41 -0500

    se incluyen la sfunciones de velocidad y update de A^

--- code/julia-code/dinamica_conductores.jl
+++ code/julia-code/dinamica_conductores.jl
@@ -1,30 +1,41 @@
+include("objetos.jl")
+
+# Ejemplo de función speed
+function max_speed(u::Int,
+    position_array::Array{Array{Float64,1},1}=[[0.,0.]])
+    x_array = [first(element) for element in position_array]
+if position_array[u][1] < maximum(x_array)/2.
+    return 50
+else
+    return 30
+end
+end
+
+function max_speed(u::Int,v::Int, 
+    position_array::Array{Array{Float64,1},1})
+return (max_speed(u, position_array)+max_speed(v,position_array))/2
+end
+
+function speed(u::Int, 
+    position_array::Array{Array{Float64,1},1},
+    speed_memory::Dict{Int,Float64}=Dict{Int,Float64}())
+return get(speed_memory, u, max_speed(u, position_array))
+end
+
+function speed(u::Int,v::Int, 
+    position_array::Array{Array{Float64,1},1},
+    speed_memory::Dict{Int,Float64}=Dict{Int,Float64}())
+return (speed(u, position_array, speed_memory)+speed(v,position_array,speed_memory))/2
+end
+
+function update_Astarpath(Auto::auto)
+    Auto.astarpath = LightGraphs.a_star(Auto.red.digraph,
+        Auto.o, Auto.d,Auto.red.time_matrix,
+        n -> MemoryHeuristic(n, Auto.d,Auto.red.position_array,
+            Auto.h,Auto.speed_memory))
+end
+
 function BPR(tmin_ij::Float32, f_ij::Int16 ,p_ij::Float32, α::Float64 = 0.2, β::Float64 = 10.) # Funciona bien!! 
     t = tmin_ij *(1+α*(f_ij / p_ij)^β)
     return t
-end
-
-
-#TODO: El plan inicial de viaje de todos es el otorgado por el algoritmo de Dijkstra, cada auto necesita su pareja OD, incluir
-# aquí el algoritmo A*, en vez de recalcular identifica quiénes son tmin fij pij, calculamos desde la red con HASHING en los mapeos de aristas
-
-function Planes_de_viaje(Red::red,pares_OD::Array{Int32,2})
-    PV = []
-    for i in 1:length(pares_OD[:,1])
-        j,k = Dijkstra(pares_OD[i,1],pares_OD[i,2], Red)
-        push!(k,0)
-        push!(PV,k)
-    end
-    return PV
-end
-
-function restart(Autos::autos)
-    Autos.avance = zeros(Autos.N)
-    Autos.Vel = zeros(Autos.N)
-    Autos.ruta_animacion=[]
-    Autos.t_propio = zeros(Autos.N)
-    for i in 1:Autos.N #reasignación de valores en vez de push
-        o = Autos.PV[i][1]
-        Autos.arista[i,:] = [0,o]
-        push!(Autos.ruta_animacion,[])
-    end
 end
