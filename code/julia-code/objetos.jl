d637e997d912b15032c78310f678d9dce7859dd3
Mariana Blaz <luzblaz@ciencias.unam.mx>
2021-04-23 17:41:56 -0500

    agrega tiempo de salida e informacion del ultimo nodo visitado y la distancia desde este a los autos

--- code/julia-code/objetos.jl
+++ code/julia-code/objetos.jl
@@ -10,23 +10,34 @@ mutable struct network
 end
 
 mutable struct auto
-    #origen y destino, agregar tiempo de salida
+    #origen, destino y tiempo de salida
     o::Int64
     d::Int64
+    ts::Float64
+
     #constante h 
     h::Float64
+
     #Red
     red::network
+
     #memoria de las velocidades en los nodos
     speed_memory::Dict{Int64,Float64}
+
     #camino A*
     astarpath::Array{LightGraphs.SimpleGraphs.SimpleEdge{Int64},1}
+
+    ##Información del nodo del que acaban de salir y el avance respecto a este
+    last_node::Int64
+    avance::Float64
     
-    function auto(o::Int64,d::Int64,h::Float64,Red::network)
+    function auto(o::Int64, d::Int64, ts::Float64, h::Float64,Red::network)
         speed_memory = Dict{Int64, Float64}()
         Astarpath=LightGraphs.a_star(Red.digraph,
             o, d,Red.time_matrix,n -> TimeEuclideanHeuristic(n,
                 d,Red.position_array))
-        new(o,d,h,Red,speed_memory,Astarpath)
+        last_node = o
+        avance = 0.
+        new(o,d,ts,h,Red,speed_memory,Astarpath,last_node,avance)
     end
 end
