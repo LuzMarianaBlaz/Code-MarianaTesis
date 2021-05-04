3a49469ecd5b01f22f874bad96ef80882ad8a8ff
Mariana Blaz <luzblaz@ciencias.unam.mx>
2021-05-04 17:43:44 -0500

    uses city_matrix instead of time_matrix, adds a boolean to indicate if the car is already out

--- code/julia-code/objetos.jl
+++ code/julia-code/objetos.jl
@@ -1,11 +1,11 @@
 mutable struct network
     digraph::SimpleDiGraph{Int64}
     position_array::Array{Array{Float64,1},1}
-    time_matrix::Array{Float64,2}
+    city_matrix::Array{Float64,3} 
     function network(digraph::SimpleDiGraph{Int64},
             position_array::Array{Array{Float64,1},1},
-            weight_matrix::Array{Float64,2})
-        new(digraph,position_array,weight_matrix)
+            city_matrix::Array{Float64,3})
+        new(digraph,position_array,city_matrix)
     end
 end
 
@@ -27,14 +27,18 @@ mutable struct auto
     ##Información del nodo del que acaban de salir y el avance respecto a este
     last_node::Int64
     avance::Float64
+
+    #si el auto ya salió o no
+    is_out::Bool
     
     function auto(o::Int64, d::Int64, ts::Float64, h::Float64,Red::network)
         speed_memory = Dict{Int64, Float64}()
         Astarpath=LightGraphs.a_star(Red.digraph,
-            o, d,Red.time_matrix,n -> TimeEuclideanHeuristic(n,
+            o, d,red_cuadrada.city_matrix[:,:,3],n -> TimeEuclideanHeuristic(n,
                 d,Red.position_array))
         last_node = o
         avance = 0.
-        new(o,d,ts,h,speed_memory,Astarpath,last_node,avance)
+        is_out = false
+        new(o,d,ts,h,speed_memory,Astarpath,last_node,avance,is_out)
     end
 end
