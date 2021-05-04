67cf23196533063f18f36de482fca69483afdc32
Mariana Blaz <luzblaz@ciencias.unam.mx>
2021-05-04 17:52:02 -0500

    correccion de los indices del arreglo city_matrix

--- code/julia-code/objetos.jl
+++ code/julia-code/objetos.jl
@@ -34,7 +34,7 @@ mutable struct auto
     function auto(o::Int64, d::Int64, ts::Float64, h::Float64,Red::network)
         speed_memory = Dict{Int64, Float64}()
         Astarpath=LightGraphs.a_star(Red.digraph,
-            o, d,red_cuadrada.city_matrix[:,:,3],n -> TimeEuclideanHeuristic(n,
+            o, d,red_cuadrada.city_matrix[:,:,1],n -> TimeEuclideanHeuristic(n,
                 d,Red.position_array))
         last_node = o
         avance = 0.
