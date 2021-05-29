8c22a7ee705157b41c4ae2b8acc593207e55a717
Mariana Blaz <luzblaz@ciencias.unam.mx>
2021-05-28 20:13:15 -0500

    agrega la función siguiente tiempo de salida

--- code/julia-code/dinamica_conductores.jl
+++ code/julia-code/dinamica_conductores.jl
@@ -39,3 +39,39 @@ function BPR(tmin_ij::Float64, f_ij::Float64 ,p_ij::Float64, α::Float64 = 0.2, 
     t = tmin_ij *(1+α*(f_ij / p_ij)^β)
     return t
 end
+
+function sig_ts(tiempo_universal::Float64, Red::network, Autos::Array{auto,1})
+    
+    Autos = [auto for auto in Autos if !(auto.is_out)]
+    p=sortperm([auto.ts for auto in Autos])
+    Autos = Autos[p]
+
+    if length(Autos) > 0
+        u = Autos[1].o
+        v = dst(Autos[1].astarpath[1])
+        #si la calle a la que debe salir tiene espacio:
+        if Red.city_matrix[u,v,3] < Red.city_matrix[u,v,2] 
+            sts = Autos[1].ts-tiempo_universal
+            car = Autos[1]
+        else 
+            for auto in Autos 
+                if auto.o == u & v == dst(auto.astarpath[1])
+                    auto.ts = auto.ts + 10.
+                end
+            end
+                    
+            p=sortperm([auto.ts for auto in Autos])
+            Autos = Autos[p]
+            if Autos != Autos[p]
+                sts, car = sig_ts(tiempo_universal, Red, Autos)
+            else
+                sts=Inf
+                car = nothing 
+            end
+        end
+    else
+        sts = Inf
+        car = nothing
+    end
+    return sts, car
+end
