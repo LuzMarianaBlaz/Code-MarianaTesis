6c6b3a83dcaf6454dca2fc75992f0e66ea41fe6d
Mariana Blaz <luzblaz@ciencias.unam.mx>
2021-12-16 08:30:30 -0600

    pone el archivo bash igual que el que esta en el servidor

--- code/julia-code/Mariana-Paquete/Mariana-Bash/mariana-simulation.sh
+++ code/julia-code/Mariana-Paquete/Mariana-Bash/mariana-simulation.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
 
-source mariana-iterators.sh
-cd
-HOME=$(pwd)
-cd Documents/Code-MarianaTesis/code/julia-code/
-mkdir -p Mariana-Sim
-cd Mariana-Sim
+source iterators.sh
 
-BASE="$HOME/Documents/Code-MarianaTesis/code/julia-code/Mariana-Sim"
+HOMEM="/home/aramis/Mariana"
+cd $HOMEM
+mkdir -p Sim
+
+BASE="${HOMEM}/Sim"
 cd $BASE
 
+declare -a arrR=${redsizes[*]}
+declare -a arrA=${autosnums[*]}
+
 ## Hacer un ciclo para todos los tamaños de red y n_autos deseados
-for tamano_red in ${redsizes}
-    do 
-        mkdir 'redsize'${tamano_red}
-        cd redsize${tamano_red}
+for tamano_red in ${arrR}
+do 
+    cd $BASE
+    
+    mkdir -p 'redsize'${tamano_red}
+    cd redsize${tamano_red}
+    
+    for n_cars in ${arrA}
+    do
+        
+        cd $BASE/redsize${tamano_red}/
+        mkdir -p 'nautos'${n_cars}
+        cd nautos${n_cars}
+        
+        # copiar el archivo de simulación
+        cp ${HOMEM}/Paquete/mariana-simulation.jl mariana-simulation-RS${tamano_red}-N${n_cars}.jl
+        
+        # cambiar los parametros del archivo
+        sed -i -e 's/TAMANORED/'$tamano_red'/'  mariana-simulation-RS${tamano_red}-N${n_cars}.jl 
+        sed -i -e 's/NCARS/'$n_cars'/'  mariana-simulation-RS${tamano_red}-N${n_cars}.jl
+        
+        # correr el archivo y guardar el log
+        for repetition in {1..10}
+        do
+            cd $BASE/redsize${tamano_red}/nautos${n_cars}/
+            mkdir -p ${repetition}
 
-        for n_cars in ${autosnums}
-            do
-                mkdir 'nautos'${n_cars}
-                cd nautos${n_cars}
-                # copiar el archivo de simulación
-                cp $HOME/Documents/Code-MarianaTesis/code/julia-code/Mariana-Paquete/mariana-simulation.jl ./mariana-simulation-${tamano_red}-${n_cars}.jl
-                # cambiar los parametros del archivo
-                sed -i -e 's/TAMANORED/'$tamano_red'/'  mariana-simulation-${tamano_red}-${n_cars}.jl 
-                sed -i -e 's/NCARS/'$n_cars'/'  mariana-simulation-${tamano_red}-${n_cars}.jl
-                # correr el archivo y guardar el log
-                for repetition in {0..3}
-                do
-                    sed -i -e 's/OUTFILEMARIANA/"out'$repetition'.csv"/'  mariana-simulation-${tamano_red}-${n_cars}.jl 
-                    #nohup $HOME/bin/julia/julia mariana-simulation-${tamano_red}-${n_cars}.jl | tee output-$repetition.log 
-                    nohup /Applications/Julia-1.5.app/Contents/Resources/julia/bin/julia mariana-simulation-${tamano_red}-${n_cars}.jl | tee output-$repetition.log 
-                    sed -i -e 's/"out'$repetition'.csv"/OUTFILEMARIANA/'  mariana-simulation-${tamano_red}-${n_cars}.jl 
+            sed 's/OUTFILEMARIANA/'Datos-RS${tamano_red}-N${n_cars}-R${repetition}.csv'/'  mariana-simulation-RS${tamano_red}-N${n_cars}.jl >> mariana-simulation-RS${tamano_red}-N${n_cars}-R${repetition}.jl 
+            mv mariana-simulation-RS${tamano_red}-N${n_cars}-R${repetition}.jl ${repetition}/
 
-                done
-                cd ..
-            done
-        cd ..
+            export BASEDIR=${BASE}/redsize${tamano_red}/nautos${n_cars}/${repetition}/
+            cd $BASEDIR
+
+            echo $(pwd) mariana-simulation-RS${tamano_red}-N${n_cars}-R${repetition}.jl 
+            nohup /home/aramis/bin/julia/julia mariana-simulation-RS${tamano_red}-N${n_cars}-R${repetition}.jl | tee output.log &
+        done
     done
+done
